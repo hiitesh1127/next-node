3ab0e4132c0d79ac635e47bf7316d32be2767bf1
hiitesh1127 <72217750+hiitesh1127@users.noreply.github.com>
2022-02-11 20:42:17 +0530

    adding typescript class

--- src/api/routes/authRoutes.ts
+++ src/api/routes/authRoutes.ts
@@ -1,66 +1,85 @@
-import {Request, Response, NextFunction} from 'express'
-import createHttpError from 'http-errors';
+import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
 import { Router } from "express";
- 
-import User from '../models/userModel'
-import {userSchema} from '../helpers/validation'
-import {signAccessToken, signRefreshToken, verifyRefreshToken} from '../helpers/jwt'
 
-const router = Router()
+import User from "../models/userModel";
+import { userSchema } from "../helpers/validation";
+import Authentication from "../helpers/jwt";
 
-router.post('/signup', async(req : Request, res : Response, next : NextFunction) => {
+class AuthRoutes {
+  public router = Router();
+  public auth = new Authentication();
+  constructor() {
+    this.authRoutes();
+  }
+
+  private authRoutes() {
+    this.router.post("/signup", this.signup);
+    this.router.post("/login", this.login);
+    this.router.post("/refresh", this.refresh);
+  }
+
+  private signup = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await userSchema.validateAsync(req.body)
-        const currentUser = await User.findOne({email : result.email});
+      const result = await userSchema.validateAsync(req.body);
+      const currentUser = await User.findOne({ email: result.email });
 
-        if(currentUser)
-            throw new createHttpError.BadRequest("already exist");
+      if (currentUser) throw new createHttpError.BadRequest("already exist");
 
-        const user = new User(result);
-        const saveUser = await user.save()
-        const accessToken = await signAccessToken(saveUser.id, saveUser.email)
-        const refreshToken = await signRefreshToken(saveUser.id, saveUser.email)
+      const user = new User(result);
+      const saveUser = await user.save();
+      const accessToken = await this.auth.signAccessToken(
+        saveUser.id,
+        saveUser.email
+      );
+      const refreshToken = await this.auth.signRefreshToken(
+        saveUser.id,
+        saveUser.email
+      );
 
-        res.send({accessToken, refreshToken})
-
+      res.send({ accessToken, refreshToken });
     } catch (error) {
-        next(error)
+      next(error);
     }
-})
+  };
 
-router.post('/login', async(req, res, next) => {
+  private login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await userSchema.validateAsync(req.body)
-        const user = await User.findOne({email : result.email})
-        if(!user)
-            throw new createHttpError.NotFound('Email is not registered')
-        const password = result.password;
-        if(password != user.password)
-            throw new createHttpError.Unauthorized('Username/Password is not correct')
-        const accessToken = await signAccessToken(user.id, user.email)
-        const refreshToken = await signRefreshToken(user.id, user.email)
+      const result = await userSchema.validateAsync(req.body);
+      const user = await User.findOne({ email: result.email });
+      if (!user) throw new createHttpError.NotFound("Email is not registered");
+      const password = result.password;
+      if (password != user.password)
+        throw new createHttpError.Unauthorized(
+          "Username/Password is not correct"
+        );
+      const accessToken = await this.auth.signAccessToken(user.id, user.email);
+      const refreshToken = await this.auth.signRefreshToken(
+        user.id,
+        user.email
+      );
 
-        res.send({accessToken, refreshToken})
+      res.send({ accessToken, refreshToken });
     } catch (error) {
-        next(error);
+      next(error);
     }
-}) 
+  };
 
-router.post('/refresh', async(req, res, next) => {
+  private refresh = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {refreshToken} = req.body
-        console.log(refreshToken)
-        if(!refreshToken)
-            throw new createHttpError.BadRequest();
-        const user : any = await verifyRefreshToken(refreshToken)
-        console.log(user)
-        const accessToken = await signAccessToken(user.id, user.email)
-        const refToken = await signRefreshToken(user.id, user.email)
+      const { refreshToken } = req.body;
+      console.log(refreshToken);
+      if (!refreshToken) throw new createHttpError.BadRequest();
+      const user: any = await this.auth.verifyRefreshToken(refreshToken);
+      console.log(user);
+      const accessToken = await this.auth.signAccessToken(user.id, user.email);
+      const refToken = await this.auth.signRefreshToken(user.id, user.email);
 
-        res.send({accessToken, refreshToken : refToken})
-    } catch(error) {
-        next(error)
+      res.send({ accessToken, refreshToken: refToken });
+    } catch (error) {
+      next(error);
     }
-})
+  };
+}
 
-export default router;
+export default AuthRoutes;
